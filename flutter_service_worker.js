c6af8430a1eaef9be6c6914ed85aebe9faaabb71
Enio Luciano <enioluciano@MacBook-Pro-de-Enio.local>
2021-06-14 17:13:36 -0300

    web fonts  done  :boom:

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "97fcadc477150b4d7087c9ebf5daa3e7",
-"index.html": "0b2374218b8437b201d2baf6630fcac6",
-"/": "0b2374218b8437b201d2baf6630fcac6",
-"main.dart.js": "171163bdf1705b78edffa91e36c67e0d",
+"index.html": "b71f5b6cb209353662f4009c386bbd24",
+"/": "b71f5b6cb209353662f4009c386bbd24",
+"main.dart.js": "d294465bed1aaadfbec89860953dbbf2",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
